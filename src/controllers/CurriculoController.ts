2c1efa71e84e35b2c9680a7cb1cd3b7be4797514
Janderson Rodrigues <jandersonrodriguesir@gmail.com>
2025-12-09 13:22:51 -0300

    fix: remove processamento assíncrono de currículos ao consultar vagas

--- src/controllers/CurriculoController.ts
+++ src/controllers/CurriculoController.ts
@@ -69,10 +69,10 @@ CurriculoController.get("/vagas", async (request: Request, response: Response) =
     const curriculo = await CurriculoModel.findOne({ email })
     if (!curriculo) return response.send_notFound("Currículo não encontrado.")
 
-      console.log("Currículo encontrado. Disparando processamento de currículos...", { email, curriculo })
-    setImmediate(() => {
-      processarCurriculos()
-    })
+    //   console.log("Currículo encontrado. Disparando processamento de currículos...", { email, curriculo })
+    // setImmediate(() => {
+    //   processarCurriculos()
+    // })
 
     return response.send_ok("Vagas recuperadas com sucesso", {
       resultado: curriculo.resultado || [],
